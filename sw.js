37a3a90f1aca8d6edf77e02285681edc94dfe97a
wanne-pixel <wanne.info@gmail.com>
2026-05-30 23:56:04 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'mygym-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/mygym_app_icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
